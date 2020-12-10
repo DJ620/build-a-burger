99fb1ab96f48d17ea7f6ef8bb59e5337e7801f0b
DJ Hersh <DanielJHersh@gmail.com>
2020-12-09 17:40:55 -0800

    Set up my connection.js file and created my schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheddar Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Impossible Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bison Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
