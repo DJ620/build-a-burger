1b32616372cb9ef5acb91cb9023110c7a781981d
DJ Hersh <DanielJHersh@gmail.com>
2020-12-09 18:03:30 -0800

    Wrote my orm methods

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,39 @@
 const connection = require("../config/connection.js");
 
+const objToSql = obj => {
+    let arr = [];
+    for (let key in obj) {
+        let value = obj[key];
+        if (Object.hasOwnProperty.call(obj, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = `'${value}'`;
+            }
+            arr.push(`${key} = ${value}`);
+        };
+    };
+    return arr.toString();
+};
+
 const orm = {
-    selectAll: function() {
-        
+    selectAll: function(table, cb) {
+        let queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        let queryString = `INSERT INTO ${table} (${cols}) VALUES (${vals})`;
+        connection.query(queryString, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+    updateOne: function(table, vals, condition, cb) {
+        let queryString = `UPDATE ${table} SET ${objToSql(vals)} WHERE ${condition}`;
+        connection.query(queryString, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
     }
-}
+};
