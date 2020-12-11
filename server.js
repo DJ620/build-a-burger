e3e1e0995bde40ea1f20c376b643b669c2ec6fa5
DJ Hersh <DanielJHersh@gmail.com>
2020-12-10 16:36:00 -0800

    Set up my server.js file

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = requre("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/catsController.js");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+});
