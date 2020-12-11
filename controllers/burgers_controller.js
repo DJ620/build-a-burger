f43352b39ed0a521c26d5be702d6d7c3cfac7ba8
DJ Hersh <DanielJHersh@gmail.com>
2020-12-10 16:46:45 -0800

    Added a get route in my controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,13 @@ const burger = require("../models/burger.js");
 
 const router = express.router();
 
-
+router.get("/", (req, res) => {
+    burger.all(data => {
+        const obj = {
+            burgers: data
+        };
+        res.render("index", obj);
+    });
+});
 
 module.exports = router;
