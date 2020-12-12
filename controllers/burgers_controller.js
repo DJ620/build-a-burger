35b226a04a13f4839615063cdae444d803f1a94a
DJ Hersh <DanielJHersh@gmail.com>
2020-12-11 18:39:16 -0800

    Commented mt burgers_controller.js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,23 +1,29 @@
+// Requiring the express dependency and the burger model
 const express = require("express");
 const burger = require("../models/burger.js");
 
+// Defining my router for the API calls
 const router = express.Router();
 
+// API get route
 router.get("/", (req, res) => {
     burger.all(data => {
         const obj = {
             burgers: data
         };
+        // Sends back the index.handlebars file
         res.render("index", obj);
     });
 });
 
+// API post route
 router.post("/api/burgers", (req, res) => {
     burger.insert(["burger_name"], [req.body.burger_name], result => {
         res.json({ id: result.insertId });
     });
 });
 
+// API put route
 router.put("/api/burgers/:id", (req, res) => {
     const condition = "id = " + req.params.id;
     burger.update({devoured: true}, condition, result => {
@@ -28,4 +34,5 @@ router.put("/api/burgers/:id", (req, res) => {
     });
 });
 
+// Exporting the router to be used in other files
 module.exports = router;
