836167db86c5f4363ac95c21edf7771e2d7f151e
DJ Hersh <DanielJHersh@gmail.com>
2020-12-11 18:32:29 -0800

    Commented my server.js file

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
+// Requiring my dependencies
 const express = require("express");
 const dotenv = require("dotenv").config();
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgers_controller.js");
 
+// Creating my express server
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+//Handlebars middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Telling my server to use the api routes in the burgers_controller.js file
 app.use(routes);
 
+// Spinning up my server when the file is run
 app.listen(PORT, function() {
     console.log("App now listening at localhost:" + PORT);
 });
